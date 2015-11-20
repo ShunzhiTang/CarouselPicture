73a07bcacbedcdba10cc2a6e81fa522ad8bd9513
tangShunZhi <1046975221@qq.com>
2015-11-20 01:26:47 -0800

    1、init初始化

---
+++ CarouselPicture/CarouselPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarouselPicture
+//
+//  Created by Tsz on 15/11/20.
+//  Copyright © 2015年 Tsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
